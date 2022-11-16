d4a8dd63b36bcac773d0964a34efca93742b6963
jasmine-ball <jazzie82@gmail.com>
2022-11-16 15:41:30 +1100

    Add return at appropriate point to stop the search at specified limit

--- kanban.sh
+++ kanban.sh
@@ -78,19 +78,20 @@ esac
 
 #Send data to file
 process_user_input_fun () {
-  echo $4 $3 $1 $2 >> kanban.txt
+  echo $4 $5 $3 $1 $2 >> kanban.txt
 }
 
 #Add entries
 add_entries_fun () {
   read -p "$HG2"Title"$Z: " -e title 
   read -p "$HG2"Description"$Z: " -e description 
-  read -p "$HG2"Tags"$Z: " -e tags 
+  read -p "$HG2"Tags"$Z: " -e tags
+  read -p "$HG2"Priority"$Z: " -i "P01" -e pr
   title=${title//' '/-}
   description=${description//' '/-}
-  if [ ${#tags} == 0 ]; then tags='#p4'; else tags='#'${tags//' '/#}; fi
+  if [ ${#tags} == 0 ]; then tags='#other'; else tags='#'${tags//' '/#}; fi
   id=$(date +%y%m%d%H%M%S)
-  echo $id $tags $title $description >> kanban.txt
+  echo $id $pr $tags $title $description >> kanban.txt
 }
 
 #Filter
@@ -122,18 +123,20 @@ update_entries_fun () {
 
   if [ ${#selected_id} == 12 ]
     then
-      title=$(sed -n /.*$id.*/p kanban.txt | cut -d ' ' -f 3)
-      description=$(sed -n /.*$id.*/p kanban.txt | cut -d ' ' -f 4)
-      tags=$(sed -n /.*$id.*/p kanban.txt | cut -d ' ' -f 2 | cut -d ' ' -f 2)
+      pr=$(sed -n /.*$id.*/p kanban.txt | cut -d ' ' -f 2)
+      title=$(sed -n /.*$id.*/p kanban.txt | cut -d ' ' -f 4)
+      description=$(sed -n /.*$id.*/p kanban.txt | cut -d ' ' -f 5)
+      tags=$(sed -n /.*$id.*/p kanban.txt | cut -d ' ' -f 3)
       read -p "$HG2"ID"$Z: " -i $selected_id -e selected_id
       read -p "$HG2"Title"$Z: " -i $title -e title
       read -p "$HG2"Description"$Z: " -i $description -e description
       read -p "$HG2"Tags"$Z: " -i $tags -e tags
+      read -p "$HG2"Priority"$Z: " -i $pr -e pr
       sed -i /${selected_id}/d kanban.txt
       title=${title//' '/-}
       description=${description//' '/-}
       tags=${tags//' '/#}
-      process_user_input_fun $title $description $tags $selected_id
+      process_user_input_fun $title $description $tags $selected_id $pr
     else
       echo 'No unique match found. Please pass the search in as first parameter'
   fi
@@ -157,11 +160,11 @@ ordered_entries_fun () {
   echo "----------------------------------------"
   if [ $2 ]
     then
-      recent_entries_fun "#p0" $((2 * $max_results))
-      [ $max_results -gt 2 ] && recent_entries_fun "#started" $((4 * $max_results))      
-      recent_entries_fun "#p1" $((2 * $max_results))
-      recent_entries_fun "#p2" $((4 * $max_results))
-      recent_entries_fun "#p3" $((2 * $max_results))
+      recent_entries_fun "P00" $((4 * $max_results))
+      [ $max_results -gt 3 ] && recent_entries_fun "#started" $((4 * $max_results))      
+      recent_entries_fun "P01" $((4 * $max_results))
+      recent_entries_fun "P02" $((4 * $max_results))
+      recent_entries_fun "P03" $((2 * $max_results))
     else
       recent_entries_fun $1 $((4 * $max_results))
   fi
@@ -172,7 +175,7 @@ ordered_entries_fun () {
 #Show entries
 recent_entries_fun () {
 line_total=$(sed -n '$=' kanban.txt)
-line_count=1
+line_count=0
 for i in `seq $line_total -1 1`; 
   do
     if [ $line_count -le $2 ]
@@ -181,21 +184,20 @@ for i in `seq $line_total -1 1`;
        if [ $1 ]
          then
            line_id=$(sed -n ${i}p kanban.txt | tr 'A-Z' 'a-z' | grep ${1,,} | cut -d ' ' -f 1)
-           if [ ${#line_id} == 12 ]; then title=$(sed -n ${i}p kanban.txt | grep ${line_id} | cut -d ' ' -f 3); fi
+           if [ ${#line_id} == 12 ]; then title=$(sed -n ${i}p kanban.txt | grep ${line_id} | cut -d ' ' -f 4); fi
          else 
-           title=$(sed -n ${i}p kanban.txt | cut -d ' ' -f 3);
+           title=$(sed -n ${i}p kanban.txt | cut -d ' ' -f 4);
        fi
          
-         descr=$(sed -n ${i}p kanban.txt | cut -d ' ' -f 4-)
-         task_status_p1=$(sed -n ${i}p kanban.txt | grep -c '#p1')
-         task_status_p0=$(sed -n ${i}p kanban.txt | grep -c '#p0')
-         task_status_p2=$(sed -n ${i}p kanban.txt | grep -c '#p2')
-         task_status_started=$(sed -n ${i}p kanban.txt | grep -c '#started')
-         task_status_p3=$(sed -n ${i}p kanban.txt | grep -c '#p3')
+         descr=$(sed -n ${i}p kanban.txt | cut -d ' ' -f 5-)
+         task_status_p1=$(sed -n ${i}p kanban.txt | grep -c 'P01')
+         task_status_p0=$(sed -n ${i}p kanban.txt | grep -c 'P00')
+         task_status_p2=$(sed -n ${i}p kanban.txt | grep -c 'P02')
+         task_status_started=$(sed -n ${i}p kanban.txt | grep -c 'P98')
+         task_status_p3=$(sed -n ${i}p kanban.txt | grep -c 'P03')
     
        if [ $line_id ] && [ $line_count -le $2 ]
          then
-           line_count=$((line_count +1))
            color=$HG70
            if [ $task_status_p1 -gt 0 ]; then color=$P1; fi
            if [ $task_status_p0 -gt 0 ]; then color=$P0; fi
@@ -203,7 +205,10 @@ for i in `seq $line_total -1 1`;
            if [ $task_status_started -gt 0 ]; then color=$ST; fi
            if [ $task_status_p3 -gt 0 ]; then color=$P3; fi
            echo " ${color}${title//-/ } ${Z}${descr//-/ }" | cut -b -${results_width}
-    
+           line_count=$((line_count +1))
+       elif [ $line_count -ge 1 ]
+         then
+           return        
        fi
     fi
   done
@@ -238,3 +243,6 @@ elif [ $1 ]
 else
     show_menu_fun
 fi
+
+#Sorting to allow grouping of cards for performance
+sort -r -k 2 kanban.txt -o kanban.txt
